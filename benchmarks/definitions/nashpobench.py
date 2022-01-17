2330c2803b7a80dd644048e7908bc44afc2666f3
Aaron Klein <kleiaaro@amazon.com>
2022-01-17 11:50:04 +0100

    int encoding

--- benchmarks/definitions/nashpobench.py
+++ benchmarks/definitions/nashpobench.py
@@ -10,27 +10,26 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
-
-from sklearn.ensemble import RandomForestRegressor
+from pathlib import Path
 
 from syne_tune.search_space import choice, randint, uniform, loguniform
-
+from typing import Dict, Optional
 from blackbox_repository.conversion_scripts.scripts.fcnet_import import \
     METRIC_ELAPSED_TIME, METRIC_VALID_LOSS, RESOURCE_ATTR, BLACKBOX_NAME
-
 
 _config_space = {
     "hp_activation_fn_1": choice(["tanh", "relu"]),
     "hp_activation_fn_2": choice(["tanh", "relu"]),
-    "hp_batch_size": randint(8, 64),
-    "hp_dropout_1": uniform(0.0, 0.6),
-    "hp_dropout_2": uniform(0.0, 0.6),
-    "hp_init_lr": loguniform(0.0005, 0.1),
+    "hp_batch_size": randint(0, 3),
+    "hp_dropout_1": randint(0, 2),
+    "hp_dropout_2": randint(0, 2),
+    "hp_init_lr": randint(0, 5),
     'hp_lr_schedule': choice(["cosine", "const"]),
-    'hp_n_units_1': randint(16, 512),
-    'hp_n_units_2': randint(16, 512),
+    # 'hp_n_units_1': choice([16, 32, 64, 128, 256, 512]),
+    # 'hp_n_units_2': choice([16, 32, 64, 128, 256, 512]),
+    'hp_n_units_1': randint(0, 5),
+    'hp_n_units_2': randint(0, 5),
 }
-
 
 def nashpobench_default_params(params=None):
     dont_sleep = str(
@@ -61,7 +60,8 @@ def nashpobench_benchmark(params):
         dont_sleep=params['dont_sleep'],
         blackbox_repo_s3_root=params.get('blackbox_repo_s3_root'))
     return {
-        'script': None,
+        'script': Path(__file__).parent.parent.parent / "examples" /
+                  "training_scripts" / "nashpobench" / "nashpobench.py",
         'metric': METRIC_VALID_LOSS,
         'mode': 'min',
         'resource_attr': RESOURCE_ATTR,
@@ -70,8 +70,9 @@ def nashpobench_benchmark(params):
         'config_space': config_space,
         'cost_model': None,
         'supports_simulated': True,
-        'blackbox_name': BLACKBOX_NAME,
-        'surrogate': RandomForestRegressor(max_samples=0.005, bootstrap=True),
+        'blackbox_name': None,
+        # 'surrogate': Wrapper(),
+        # 'do_normalize': False,
         'time_this_resource_attr': METRIC_ELAPSED_TIME,
     }
 
