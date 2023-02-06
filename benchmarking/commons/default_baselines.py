63eec5a86afb21558a9b750cf4f31284890ef7bf
Matthias Seeger <matthis@amazon.de>
2023-02-06 09:03:39 +0100

    Extra baselines

--- benchmarking/commons/default_baselines.py
+++ benchmarking/commons/default_baselines.py
@@ -10,6 +10,8 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
+from typing import Dict, Any
+
 from benchmarking.commons.baselines import (
     search_options,
     default_arguments,
@@ -17,217 +19,112 @@ from benchmarking.commons.baselines import (
 )
 from syne_tune.optimizer.baselines import (
     RandomSearch as _RandomSearch,
+    GridSearch as _GridSearch,
     BayesianOptimization as _BayesianOptimization,
-    DyHPO as _DyHPO,
+    KDE as _KDE,
+    BORE as _BORE,
+    BoTorch as _BoTorch,
+    REA as _REA,
+    ConstrainedBayesianOptimization as _ConstrainedBayesianOptimization,
     ASHA as _ASHA,
     MOBSTER as _MOBSTER,
     HyperTune as _HyperTune,
     BOHB as _BOHB,
+    DyHPO as _DyHPO,
+    ASHABORE as _ASHABORE,
     SyncHyperband as _SyncHyperband,
     SyncBOHB as _SyncBOHB,
     DEHB as _DEHB,
     SyncMOBSTER as _SyncMOBSTER,
-    KDE as _KDE,
 )
 from syne_tune.util import recursive_merge
 
 
-def RandomSearch(method_arguments: MethodArguments, **kwargs):
-    return _RandomSearch(
-        **recursive_merge(
-            default_arguments(
-                method_arguments,
-                dict(
-                    config_space=method_arguments.config_space,
-                    search_options=search_options(method_arguments),
-                ),
-            ),
-            kwargs,
-            stop_keys=["config_space"],
-        )
+def _baseline_kwargs(
+    method_arguments: MethodArguments,
+    kwargs: Dict[str, Any],
+    is_multifid: bool = False,
+) -> Dict[str, Any]:
+    da_input = dict(
+        config_space=method_arguments.config_space,
+        search_options=search_options(method_arguments),
     )
+    if is_multifid:
+        da_input["resource_attr"] = method_arguments.resource_attr
+    return recursive_merge(
+        default_arguments(method_arguments, da_input),
+        kwargs,
+        stop_keys=["config_space"],
+    )
+
+
+def RandomSearch(method_arguments: MethodArguments, **kwargs):
+    return _RandomSearch(**_baseline_kwargs(method_arguments, kwargs))
+
+
+def GridSearch(method_arguments: MethodArguments, **kwargs):
+    return _GridSearch(**_baseline_kwargs(method_arguments, kwargs))
 
 
 def BayesianOptimization(method_arguments: MethodArguments, **kwargs):
-    return _BayesianOptimization(
-        **recursive_merge(
-            default_arguments(
-                method_arguments,
-                dict(
-                    config_space=method_arguments.config_space,
-                    search_options=search_options(method_arguments),
-                ),
-            ),
-            kwargs,
-            stop_keys=["config_space"],
-        )
-    )
-
-
-def ASHA(method_arguments: MethodArguments, **kwargs):
-    return _ASHA(
-        **recursive_merge(
-            default_arguments(
-                method_arguments,
-                dict(
-                    config_space=method_arguments.config_space,
-                    search_options=search_options(method_arguments),
-                    resource_attr=method_arguments.resource_attr,
-                ),
-            ),
-            kwargs,
-            stop_keys=["config_space"],
-        )
-    )
-
-
-def DyHPO(method_arguments: MethodArguments, **kwargs):
-    return _DyHPO(
-        **recursive_merge(
-            default_arguments(
-                method_arguments,
-                dict(
-                    config_space=method_arguments.config_space,
-                    search_options=search_options(method_arguments),
-                    resource_attr=method_arguments.resource_attr,
-                ),
-            ),
-            kwargs,
-            stop_keys=["config_space"],
-        )
-    )
-
-
-def HyperTune(method_arguments: MethodArguments, **kwargs):
-    return _HyperTune(
-        **recursive_merge(
-            default_arguments(
-                method_arguments,
-                dict(
-                    config_space=method_arguments.config_space,
-                    search_options=search_options(method_arguments),
-                    resource_attr=method_arguments.resource_attr,
-                ),
-            ),
-            kwargs,
-            stop_keys=["config_space"],
-        )
-    )
-
-
-def MOBSTER(method_arguments: MethodArguments, **kwargs):
-    return _MOBSTER(
-        **recursive_merge(
-            default_arguments(
-                method_arguments,
-                dict(
-                    config_space=method_arguments.config_space,
-                    search_options=search_options(method_arguments),
-                    resource_attr=method_arguments.resource_attr,
-                ),
-            ),
-            kwargs,
-            stop_keys=["config_space"],
-        )
-    )
-
-
-def BOHB(method_arguments: MethodArguments, **kwargs):
-    return _BOHB(
-        **recursive_merge(
-            default_arguments(
-                method_arguments,
-                dict(
-                    config_space=method_arguments.config_space,
-                    search_options=search_options(method_arguments),
-                    resource_attr=method_arguments.resource_attr,
-                ),
-            ),
-            kwargs,
-            stop_keys=["config_space"],
-        )
-    )
-
-
-def SyncHyperband(method_arguments: MethodArguments, **kwargs):
-    return _SyncHyperband(
-        **recursive_merge(
-            default_arguments(
-                method_arguments,
-                dict(
-                    config_space=method_arguments.config_space,
-                    search_options=search_options(method_arguments),
-                    resource_attr=method_arguments.resource_attr,
-                ),
-            ),
-            kwargs,
-            stop_keys=["config_space"],
-        )
-    )
-
-
-def SyncBOHB(method_arguments: MethodArguments, **kwargs):
-    return _SyncBOHB(
-        **recursive_merge(
-            default_arguments(
-                method_arguments,
-                dict(
-                    config_space=method_arguments.config_space,
-                    search_options=search_options(method_arguments),
-                    resource_attr=method_arguments.resource_attr,
-                ),
-            ),
-            kwargs,
-            stop_keys=["config_space"],
-        )
-    )
-
-
-def DEHB(method_arguments: MethodArguments, **kwargs):
-    return _DEHB(
-        **recursive_merge(
-            default_arguments(
-                method_arguments,
-                dict(
-                    config_space=method_arguments.config_space,
-                    search_options=search_options(method_arguments),
-                    resource_attr=method_arguments.resource_attr,
-                ),
-            ),
-            kwargs,
-            stop_keys=["config_space"],
-        )
-    )
-
-
-def SyncMOBSTER(method_arguments: MethodArguments, **kwargs):
-    return _SyncMOBSTER(
-        **recursive_merge(
-            default_arguments(
-                method_arguments,
-                dict(
-                    config_space=method_arguments.config_space,
-                    search_options=search_options(method_arguments),
-                    resource_attr=method_arguments.resource_attr,
-                ),
-            ),
-            kwargs,
-            stop_keys=["config_space"],
-        )
-    )
+    return _BayesianOptimization(**_baseline_kwargs(method_arguments, kwargs))
 
 
 def KDE(method_arguments: MethodArguments, **kwargs):
-    return _KDE(
-        **recursive_merge(
-            default_arguments(
-                method_arguments,
-                dict(
-                    config_space=method_arguments.config_space,
-                    search_options=search_options(method_arguments),
-                ),
-            ),
-            kwargs,
-            stop_keys=["config_space"],
-        )
-    )
+    return _KDE(**_baseline_kwargs(method_arguments, kwargs))
+
+
+def BORE(method_arguments: MethodArguments, **kwargs):
+    return _BORE(**_baseline_kwargs(method_arguments, kwargs))
+
+
+def BoTorch(method_arguments: MethodArguments, **kwargs):
+    return _BoTorch(**_baseline_kwargs(method_arguments, kwargs))
+
+
+def REA(method_arguments: MethodArguments, **kwargs):
+    return _REA(**_baseline_kwargs(method_arguments, kwargs))
+
+
+def ConstrainedBayesianOptimization(method_arguments: MethodArguments, **kwargs):
+    return _ConstrainedBayesianOptimization(**_baseline_kwargs(method_arguments, kwargs))
+
+
+def ASHA(method_arguments: MethodArguments, **kwargs):
+    return _ASHA(**_baseline_kwargs(method_arguments, kwargs, is_multifid=True))
+
+
+def MOBSTER(method_arguments: MethodArguments, **kwargs):
+    return _MOBSTER(**_baseline_kwargs(method_arguments, kwargs, is_multifid=True))
+
+
+def HyperTune(method_arguments: MethodArguments, **kwargs):
+    return _HyperTune(**_baseline_kwargs(method_arguments, kwargs, is_multifid=True))
+
+
+def BOHB(method_arguments: MethodArguments, **kwargs):
+    return _BOHB(**_baseline_kwargs(method_arguments, kwargs, is_multifid=True))
+
+
+def DyHPO(method_arguments: MethodArguments, **kwargs):
+    return _DyHPO(**_baseline_kwargs(method_arguments, kwargs, is_multifid=True))
+
+
+def ASHABORE(method_arguments: MethodArguments, **kwargs):
+    return _ASHABORE(**_baseline_kwargs(method_arguments, kwargs, is_multifid=True))
+
+
+def SyncHyperband(method_arguments: MethodArguments, **kwargs):
+    return _SyncHyperband(**_baseline_kwargs(method_arguments, kwargs, is_multifid=True))
+
+
+def SyncBOHB(method_arguments: MethodArguments, **kwargs):
+    return _SyncBOHB(**_baseline_kwargs(method_arguments, kwargs, is_multifid=True))
+
+
+def DEHB(method_arguments: MethodArguments, **kwargs):
+    return _DEHB(**_baseline_kwargs(method_arguments, kwargs, is_multifid=True))
+
+
+def SyncMOBSTER(method_arguments: MethodArguments, **kwargs):
+    return _SyncMOBSTER(**_baseline_kwargs(method_arguments, kwargs, is_multifid=True))
