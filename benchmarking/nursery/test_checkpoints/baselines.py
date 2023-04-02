7f7e0f5ab1af31c84590db71f8b927ca80d622c4
Matthias Seeger <matthis@amazon.de>
2023-04-02 19:39:49 +0200

    Change experimental setup

--- benchmarking/nursery/test_checkpoints/baselines.py
+++ benchmarking/nursery/test_checkpoints/baselines.py
@@ -17,9 +17,8 @@ from benchmarking.commons.default_baselines import (
 
 class Methods:
     ASHA = "ASHA"
-    ASHA_RCP_DEF = "ASHA-RCP-DEF"
+    ASHA_RCP_10 = "ASHA-RCP-10"
     ASHA_RCP_5 = "ASHA-RCP-5"
-    ASHA_RCP_20 = "ASHA-RCP-20"
     ASHA_RCP_30 = "ASHA-RCP-30"
     ASHA_RCP_BASE_RANDOM = "ASHA-RCP-BASE-RANDOM"
     ASHA_RCP_BASE_BYLEVEL = "ASHA-RCP-BASE-BYLEVEL"
@@ -27,11 +26,12 @@ class Methods:
 
 methods = {
     Methods.ASHA: lambda method_arguments: ASHA(method_arguments, type="promotion"),
-    Methods.ASHA_RCP_DEF: lambda method_arguments: ASHA(
+    Methods.ASHA_RCP_10: lambda method_arguments: ASHA(
         method_arguments,
         type="promotion",
         early_checkpoint_removal_kwargs=dict(
             max_num_checkpoints=10,
+            approx_steps=10,
         ),
     ),
     Methods.ASHA_RCP_5: lambda method_arguments: ASHA(
@@ -40,14 +40,6 @@ methods = {
         early_checkpoint_removal_kwargs=dict(
             max_num_checkpoints=10,
             approx_steps=5,
-        ),
-    ),
-    Methods.ASHA_RCP_20: lambda method_arguments: ASHA(
-        method_arguments,
-        type="promotion",
-        early_checkpoint_removal_kwargs=dict(
-            max_num_checkpoints=10,
-            approx_steps=20,
         ),
     ),
     Methods.ASHA_RCP_30: lambda method_arguments: ASHA(
@@ -74,8 +66,4 @@ methods = {
             baseline="by_level",
         ),
     ),
-}
-
-methods = {
-    k: methods[k] for k in [Methods.ASHA_RCP_BASE_RANDOM, Methods.ASHA_RCP_BASE_BYLEVEL]
 }
