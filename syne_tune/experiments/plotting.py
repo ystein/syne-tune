8274d4dca7122529c10194e7de82c98276f390af
Matthias Seeger <matthis@amazon.de>
2023-04-12 18:18:49 +0200

    Defined plot parameters and class

--- syne_tune/experiments/plotting.py
+++ syne_tune/experiments/plotting.py
@@ -10,9 +10,10 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
-from typing import Dict, Any, Optional, Tuple, Callable, Union, List, Set
+from typing import Dict, Any, Optional, Tuple, Callable, Union, List
 import itertools
 import json
+from dataclasses import dataclass
 
 import pandas as pd
 
@@ -23,6 +24,8 @@ from syne_tune.util import experiment_path
 MapMetadataToSetup = Callable[[Dict[str, Any]], Optional[str]]
 
 MapMetadataToSubplot = Callable[[Dict[str, Any]], Optional[int]]
+
+DEFAULT_AGGREGATE_MODE = "median_percentiles"
 
 
 def _strip_common_prefix(tuner_path: str) -> str:
@@ -36,24 +39,37 @@ def _strip_common_prefix(tuner_path: str) -> str:
     return tuner_path[start_pos:]
 
 
+def _insert_into_dict(
+    metadata_values: Dict[str, Any], benchmark_name: str, key: str, value: Any
+):
+    if benchmark_name not in metadata_values:
+        metadata_values[benchmark_name] = dict()
+    inner_dict = metadata_values[benchmark_name]
+    if key in inner_dict:
+        inner_dict[key].append(value)
+    else:
+        inner_dict[key] = [value]
+
+
 def create_index_for_result_files(
     experiment_names: Tuple[str, ...],
-    metadata_to_setup: Dict[str, MapMetadataToSetup],
-    metadata_to_subplot: Optional[Dict[str, MapMetadataToSubplot]] = None,
+    metadata_to_setup: MapMetadataToSetup,
+    metadata_to_subplot: Optional[MapMetadataToSubplot] = None,
+    metadata_keys: Optional[List[str]] = None,
     benchmark_key: str = "benchmark",
     with_subdirs: Optional[Union[str, List[str]]] = None,
-) -> (Dict[str, List[Tuple[str, str, int]]], Set[str]):
+) -> Dict[str, Any]:
     """
     Runs over all result directories for experiments of a comparative study.
     For each experiment, we read the metadata file, extract the benchmark name
     (key ``benchmark_key``), and use ``metadata_to_setup``,
     ``metadata_to_subplot`` to map the metadata to setup name and subplot index.
-    These two mappings depend on benchmark name as well. If any of the two
-    return ``None``, the result is not used. Otherwise, we enter
-    ``(result_path, setup_name, subplot_no)`` into the list for benchmark name.
+    If any of the two return ``None``, the result is not used. Otherwise, we
+    enter ``(result_path, setup_name, subplot_no)`` into the list for benchmark
+    name.
     Here, ``result_path`` is the result path for the experiment, without the
     :meth:`~syne_tune.util.experiment_path` prefix. The index returned is the
-    dictionary from benchmark names to these list. It allows to load results
+    dictionary from benchmark names to these list. It allows loading results
     specifically for each benchmark, and we do not have to load and parse the
     metadata files again.
 
@@ -61,18 +77,28 @@ def create_index_for_result_files(
     each experiment name, if they match the expression ``with_subdirs``. Use
     ``subdirs="*"`` to be safe.
 
+    If ``metadata_keys`` is given, it contains a list of keys into the
+    metadata. In this case, a nested dictionary ``metadata_values`` is
+    returned, where ``metadata_values[benchmark_name][key]`` contains a list
+    of metadata values for this benchmark and key in ``metadata_keys``.
+
     :param experiment_names: Tuple of experiment names (prefixes, without the
         timestamps)
-    :param metadata_to_setup: See above. Dictionary from benchmark name
-    :param metadata_to_subplot: See above. Dictionary from benchmark name
+    :param metadata_to_setup: See above
+    :param metadata_to_subplot: See above. Optional
+    :param metadata_keys: See above. Optional
     :param benchmark_key: Key for benchmark in metadata files. Defaults to
         "benchmark"
     :param with_subdirs: See above
-    :return: Index, being a dictionary from benchmark name to list of
-        result paths after filtering; set of setup names encountered
+    :return: Dictionary; entry "index" for index (see above); entry
+        "setup_names" for setup names encountered; entry "metadata_values" see
+        ``metadata_keys``
     """
     reverse_index = dict()
     setup_names = set()
+    metadata_values = dict()
+    if metadata_keys is None:
+        metadata_keys = []
     for experiment_name in experiment_names:
         patterns = [experiment_name + "-*/" + ST_METADATA_FILENAME]
         if with_subdirs is not None:
@@ -97,13 +123,13 @@ def create_index_for_result_files(
                 )
                 benchmark_name = metadata[benchmark_key]
                 try:
-                    setup_name = metadata_to_setup[benchmark_name](metadata)
+                    setup_name = metadata_to_setup(metadata)
                 except BaseException as err:
                     print(f"Caught exception for {tuner_path}:\n" + str(err))
                     raise
                 if setup_name is not None:
                     if metadata_to_subplot is not None:
-                        subplot_no = metadata_to_subplot[benchmark_name](metadata)
+                        subplot_no = metadata_to_subplot(metadata)
                     else:
                         subplot_no = 0
                     if subplot_no is not None:
@@ -113,17 +139,27 @@ def create_index_for_result_files(
                             (_strip_common_prefix(tuner_path), setup_name, subplot_no)
                         )
                         setup_names.add(setup_name)
+                        for key in metadata_keys:
+                            if key in metadata:
+                                _insert_into_dict(
+                                    metadata_values, benchmark_name, key, metadata[key]
+                                )
 
-    return reverse_index, setup_names
+    result = {
+        "index": reverse_index,
+        "setup_names": setup_names,
+    }
+    if metadata_keys:
+        result["metadata_values"] = metadata_values
+    return result
 
 
 def load_results_dataframe_per_benchmark(
     experiment_list: List[Tuple[str, str, int]]
-) -> pd.DataFrame:
+) -> Optional[pd.DataFrame]:
     dfs = []
     for tuner_path, setup_name, subplot_no in experiment_list:
         tuner_path = experiment_path() / tuner_path
-        # Process time-stamped results (results.csv.zip)
         df_filename = str(tuner_path / ST_RESULTS_DATAFRAME_FILENAME)
         try:
             df = pd.read_csv(df_filename)
@@ -133,11 +169,10 @@ def load_results_dataframe_per_benchmark(
             print(f"{df_filename}: Error in pd.read_csv\n{ex}")
             df = None
         if df is None:
-            if verbose:
-                print(
-                    f"{tuner_path}: Meta-data matches filter, but "
-                    "results file not found. Skipping."
-                )
+            print(
+                f"{tuner_path}: Meta-data matches filter, but "
+                "results file not found. Skipping."
+            )
         else:
             df["setup_name"] = setup_name
             df["subplot_no"] = subplot_no
@@ -151,132 +186,172 @@ def load_results_dataframe_per_benchmark(
     return res_df
 
 
-# HIER: Anything else to extract here??
+def _impute_with_defaults(original, default, names: List[str]) -> Dict[str, Any]:
+    result = dict()
+    for name in names:
+        orig_val = getattr(original, name, None)
+        result[name] = getattr(default, name, None) if orig_val is None else orig_val
+    return result
 
 
-def load_results_by_experiment_names(
-    experiment_names: Tuple[str, ...],
-    metadata_to_setup: Callable[[dict], Optional[str]],
-    metadata_print_values: Optional[List[str]] = None,
-    metadata_to_subplot: Optional[Callable[[dict], Optional[int]]] = None,
-    verbose: bool = False,
-    with_subdirs: Optional[Union[str, List[str]]] = None,
-) -> Dict[str, Any]:
+@dataclass
+class SubplotParameters:
+    titles: List[str] = None
+    title_each_figure: bool = None
+    kwargs: Dict[str, Any] = None
+    legend_no: List[int] = None
+    xlims: List[int] = None
+
+    def merge_defaults(
+        self, default_params: "SubplotParameters"
+    ) -> "SubplotParameters":
+        new_params = _impute_with_defaults(
+            original=self,
+            default=default_params,
+            names=["titles", "title_each_figure", "kwargs", "legend_no", "xlims"],
+        )
+        if new_params["title_each_figure"] is None:
+            new_params["title_each_figure"] = False
+        return SubplotParameters(**new_params)
+
+
+@dataclass
+class PlotParameters:
+    metric: str = None
+    mode: str = None
+    xlabel: str = None
+    ylabel: str = None
+    xlim: Tuple[float, float] = None
+    ylim: Tuple[float, float] = None
+    metric_multiplier: float = None
+    tick_params: Dict[str, Any] = None
+    aggregate_mode: str = None
+    dpi: int = None
+    grid: bool = None
+    subplots: SubplotParameters = None
+
+    def merge_defaults(self, default_params: "PlotParameters") -> "PlotParameters":
+        new_params = _impute_with_defaults(
+            original=self,
+            default=default_params,
+            names=[
+                "metric",
+                "mode",
+                "xlabel",
+                "ylabel",
+                "xlim",
+                "ylim",
+                "metric_multiplier",
+                "tick_params",
+                "aggregate_mode",
+                "dpi",
+                "grid",
+            ],
+        )
+        default_values = [
+            ("metric", None),
+            ("mode", None),
+            ("metric_multiplier", 1),
+            ("aggregate_mode", DEFAULT_AGGREGATE_MODE),
+            ("dpi", 200),
+            ("grid", False),
+        ]
+        for name, def_value in default_values:
+            if new_params[name] is None:
+                assert def_value is not None, f"{name} must be given"
+                new_params[name] = def_value
+        if self.subplots is None:
+            new_params["subplots"] = default_params.subplots
+        elif default_params.subplots is None:
+            new_params["subplots"] = self.subplots
+        else:
+            new_params["subplots"] = self.subplots.merge_defaults(
+                default_params.subplots
+            )
+        return PlotParameters(**new_params)
+
+
+class ComparativeResults:
     """
-    Loads results into a dataframe similar to ``load_experiments_task``, but with
-    some differences. First, tuner names are selected by their prefixes (in
-    ``experiment_names``).
+    This class loads, processes, and plots results of a comparative study,
+    combining several experiments for different methods, seeds, and
+    benchmarks (optional). Note that an experiment corresponds to one run
+    of HPO, resulting in files :const:`~syne_tune.constants.ST_METADATA_FILENAME`
+    for metadata, and :const:`~syne_tune.constants.ST_RESULTS_DATAFRAME_FILENAME`
+    for time-stamped results.
 
-    Second, grouping and filtering is done only w.r.t. meta, so that only
-    ``metadata.json`` and ``results.csv.zip`` are needed. ``metadata_to_setup``
-    maps a metadata dict to a setup name or ``None.`` Runs mapped to ``None``
-    are to be filtered out, while runs mapped to the same setup name are samples
-    for that setup, giving rise to statistics which are plotted for that setup.
-    Here, multiple setups are compared against each other in the same plot.
+    There is one comparative plot per benchmark (aggregation of results
+    across benchmarks are not supported here). Results are grouped by
+    setup (which usually equates to method), and then summary statistics are
+    shown for each setup as function of wall-clock time. The plot can also
+    have several subplots, in which case results are first grouped into
+    subplot number, then setup.
 
-    ``metadata_to_subplot`` is optional. If given, it serves as grouping into
-    subplots. Runs mapped to None are filtered out as well.
+    Both setup name and subplot number (optional) can be configured by the
+    user, as function of metadata written for each experiment. The functions
+    ``metadata_to_setup`` and ``metadata_to_subplot`` (optional) can also be
+    used for filtering: results ofexperiments for which any of them returns
+    ``None``, are not used.
 
     If ``with_subdirs`` is given, results are loaded from subdirectories below
     each experiment name, if they match the expression ``with_subdirs``. Use
     ``subdirs="*"`` to be safe.
 
-    :param experiment_names:
-    :param metadata_to_setup:
-    :param metadata_print_values: If given, list of metadata keys. Values for
-        these keys are collected from metadata, and lists are printed
-    :param metadata_to_subplot:
+    :param experiment_names: Tuple of experiment names (prefixes, without the
+        timestamps)
+    :param setups: Possible values of setup names
+    :param num_runs: When grouping results w.r.t. benchmark name and setup
+        name, we should end up with this many experiments. They are
+        random repetitions (with different seeds), over which results are
+        aggregated
+    :param metadata_to_setup: See above
+    :param plot_params: Parameters controlling the plot. Can be overwritten
+        in :meth:`plot`
+    :param metadata_to_subplot: See above. Optional
+    :param benchmark_key: Key for benchmark in metadata files. Defaults to
+        "benchmark"
     :param with_subdirs: See above
-    :return: Dictionary with keys "results_df", "setup_names"
     """
-    dfs = []
-    setup_names = set()
-    if metadata_print_values is None:
-        metadata_print_values = []
-    for experiment_name in experiment_names:
-        patterns = [experiment_name + "-*/metadata.json"]
-        if with_subdirs is not None:
-            if not isinstance(with_subdirs, list):
-                with_subdirs = [with_subdirs]
-            pattern = patterns[0]
-            patterns = [experiment_name + f"/{x}/" + pattern for x in with_subdirs]
-        print(f"patterns = {patterns}")
-        metadata_values = {k: dict() for k in metadata_print_values}
-        for meta_path in itertools.chain(
-            *[experiment_path().glob(pattern) for pattern in patterns]
-        ):
-            tuner_path = meta_path.parent
-            try:
-                with open(str(meta_path), "r") as f:
-                    metadata = json.load(f)
-            except FileNotFoundError:
-                metadata = None
-            if metadata is not None:
-                try:
-                    setup_name = metadata_to_setup(metadata)
-                except BaseException as err:
-                    print(f"Caught exception for {tuner_path}:\n" + str(err))
-                    raise
-                if setup_name is not None:
-                    if metadata_to_subplot is not None:
-                        subplot_no = metadata_to_subplot(metadata)
-                    else:
-                        subplot_no = 0
-                    if subplot_no is not None:
-                        # Process time-stamped results (results.csv.zip)
-                        try:
-                            df = pd.read_csv(tuner_path / "results.csv.zip")
-                        except FileNotFoundError:
-                            df = None
-                        except Exception as ex:
-                            print(
-                                f"{tuner_path / 'results.csv.zip'}: Error in pd.read_csv\n{ex}"
-                            )
-                            df = None
-                        if df is None:
-                            if verbose:
-                                print(
-                                    f"{tuner_path}: Meta-data matches filter, but "
-                                    "results file not found. Skipping."
-                                )
-                        else:
-                            df["setup_name"] = setup_name
-                            setup_names.add(setup_name)
-                            df["subplot_no"] = subplot_no
-                            df["tuner_name"] = tuner_path.name
-                            # Add all metadata fields to the dataframe
-                            # for k, v in metadata.items():
-                            #    if isinstance(v, list):
-                            #        print(f"{k}: {v}")
-                            #        for i, vi in enumerate(v):
-                            #            df[k + '_%d' % i] = vi
-                            #    else:
-                            #        df[k] = v
-                            dfs.append(df)
-                            for name in metadata_print_values:
-                                if name in metadata:
-                                    dct = metadata_values[name]
-                                    value = metadata[name]
-                                    if setup_name in dct:
-                                        dct[setup_name].append(value)
-                                    else:
-                                        dct[setup_name] = [value]
 
-        if metadata_print_values:
-            parts = []
-            for name in metadata_print_values:
-                dct = metadata_values[name]
-                parts.append(f"{name}:")
-                for setup_name, values in dct.items():
-                    parts.append(f"  {setup_name}: {values}")
-            print("\n".join(parts))
+    def __init__(
+        self,
+        experiment_names: Tuple[str, ...],
+        setups: List[str],
+        num_runs: int,
+        metadata_to_setup: MapMetadataToSetup,
+        plot_params: Optional[PlotParameters] = None,
+        metadata_to_subplot: Optional[MapMetadataToSubplot] = None,
+        benchmark_key: str = "benchmark",
+        with_subdirs: Optional[Union[str, List[str]]] = None,
+    ):
+        result = create_index_for_result_files(
+            experiment_names=experiment_names,
+            metadata_to_setup=metadata_to_setup,
+            metadata_to_subplot=metadata_to_subplot,
+            benchmark_key=benchmark_key,
+            with_subdirs=with_subdirs,
+        )
+        self._reverse_index = result["index"]
+        assert result["setup_names"] == set(setups), (
+            f"Filtered results contain setup names {result['setup_names']}, "
+            f"but should contain setup names {setups}"
+        )
+        self.setups = setups
+        self.num_runs = num_runs
+        self._default_plot_params = plot_params
 
-    if not dfs:
-        res_df = None
-    else:
-        res_df = pd.concat(dfs, ignore_index=True)
-    result = {
-        "results_df": res_df,
-        "setup_names": setup_names,
-    }
-    return result
+    def plot(
+        self,
+        benchmark_name: Optional[str] = None,
+        plot_params: Optional[PlotParameters] = None,
+    ):
+        err_msg = f"benchmark_name must be one of {list(self._reverse_index.keys())}"
+        if benchmark_name is None:
+            assert len(self._reverse_index) == 1, err_msg
+            benchmark_name = next(self._reverse_index.keys())
+        else:
+            assert benchmark_name in self._reverse_index, err_msg
+        if plot_params is None:
+            plot_params = PlotParameters()
+        plot_params = plot_params.merge_defaults(self._default_plot_params)
+        # HIER!
