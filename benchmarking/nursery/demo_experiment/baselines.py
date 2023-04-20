de5e16e22846e4b2a8918376f0c4d0bdd4a72363
Matthias Seeger <matthis@amazon.de>
2023-04-20 16:13:13 +0200

    Example for experimentation with Syne Tune

---
+++ benchmarking/nursery/demo_experiment/baselines.py
@@ -0,0 +1,37 @@
+# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+from benchmarking.commons.default_baselines import (
+    ASHA,
+    MOBSTER,
+)
+
+
+class Methods:
+    ASHA = "ASHA"
+    MOBSTER = "MOBSTER"
+    ASHA_TANH = "ASHA-TANH"
+    MOBSTER_TANH = "MOBSTER-TANH"
+    ASHA_RELU = "ASHA-RELU"
+    MOBSTER_RELU = "MOBSTER-RELU"
+
+
+methods = {
+    Methods.ASHA: lambda method_arguments: ASHA(
+        method_arguments,
+        type="promotion",
+    ),
+    Methods.MOBSTER: lambda method_arguments: MOBSTER(
+        method_arguments,
+        type="promotion",
+    ),
+}
