78e29282b5b5c2246072a7a38bad3f3c17ca4e5c
aaronkl <kleinaa@cs.uni-freiburg.de>
2023-05-12 11:21:48 +0200

    formatting

--- benchmarking/nursery/nas_llm/accelerate_launcher.py
+++ benchmarking/nursery/nas_llm/accelerate_launcher.py
@@ -1,3 +1,15 @@
+# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
 import sys
 import os
 import subprocess
@@ -14,7 +26,9 @@ logger = logging.getLogger(__name__)
 # and parse_args() is used to extract them
 def parse_args():
     parser = ArgumentParser(
-        description=("SageMaker DeepSpeed Launch helper utility that will spawn deepspeed training scripts")
+        description=(
+            "SageMaker DeepSpeed Launch helper utility that will spawn deepspeed training scripts"
+        )
     )
     parser.add_argument(
         "--training_script",
@@ -44,7 +58,9 @@ def accelerate_launch(command):
 
 def main():
     train_script, config_file, args = parse_args()
-    command = f"accelerate launch --config_file {config_file} {train_script} {' '.join(args)}"
+    command = (
+        f"accelerate launch --config_file {config_file} {train_script} {' '.join(args)}"
+    )
     print(f"command = {command}")
     # launch deepspeed training
     accelerate_launch(command)
