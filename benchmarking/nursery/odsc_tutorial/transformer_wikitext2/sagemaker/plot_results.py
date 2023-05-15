b52f1a813459e891f66fff871201a84c60132f75
Matthias Seeger <matthis@amazon.de>
2023-05-15 11:55:37 +0200

    New plotting code, and removed old image files

--- benchmarking/nursery/odsc_tutorial/transformer_wikitext2/sagemaker/plot_results.py
+++ benchmarking/nursery/odsc_tutorial/transformer_wikitext2/sagemaker/plot_results.py
@@ -12,23 +12,38 @@
 # permissions and limitations under the License.
 from typing import Dict, Any, Optional
 
-from syne_tune.experiments import ComparativeResults, PlotParameters
-from benchmarking.nursery.odsc_tutorial.transformer_wikitext2.baselines import methods
+from syne_tune.experiments import ComparativeResults, PlotParameters, SubplotParameters
 from benchmarking.nursery.odsc_tutorial.transformer_wikitext2.benchmark_definitions import (
     benchmark_definitions,
 )
 
 
-SETUPS = list(methods.keys())
+TMLR10_SETUPS = [
+    "2 workers",
+    "4 workers",
+    "8 workers",
+]
 
 
 def metadata_to_setup(metadata: Dict[str, Any]) -> Optional[str]:
-    return metadata["algorithm"]
+    return f"{metadata['n_workers']} workers"
+
+
+TMLR10_METHOD_TO_SUBPLOT = {
+    "RS": 0,
+    "BO": 1,
+    "ASHA": 2,
+    "MOBSTER": 3,
+}
+
+
+def metadata_to_subplot(metadata: dict) -> Optional[int]:
+    return TMLR10_METHOD_TO_SUBPLOT[metadata["algorithm"]]
 
 
 if __name__ == "__main__":
-    experiment_names = ("odsc-1",)
-    num_runs = 10
+    experiment_names = ("tmlr-10",)
+    num_runs = 5
     download_from_s3 = False  # Set ``True`` in order to download files from S3
     # Plot parameters across all benchmarks
     plot_params = PlotParameters(
@@ -36,21 +51,29 @@ if __name__ == "__main__":
         aggregate_mode="iqm_bootstrap",
         grid=True,
     )
+    # We would like to have 4 subfigures, one for each method
+    plot_params.subplots = SubplotParameters(
+        kwargs=dict(nrows=2, ncols=2, sharex="all", sharey="all"),
+        titles=["RS", "BO", "ASHA", "MOBSTER"],
+        title_each_figure=True,
+        legend_no=[0],
+    )
     # The creation of ``results`` downloads files from S3 (only if
     # ``download_from_s3 == True``), reads the metadata and creates an inverse
     # index. If any result files are missing, or there are too many of them,
     # warning messages are printed
     results = ComparativeResults(
         experiment_names=experiment_names,
-        setups=SETUPS,
+        setups=TMLR10_SETUPS,
         num_runs=num_runs,
         metadata_to_setup=metadata_to_setup,
         plot_params=plot_params,
+        metadata_to_subplot=metadata_to_subplot,
         download_from_s3=download_from_s3,
     )
     # Create comparative plot (single panel)
     benchmark_name = "transformer_wikitext2"
-    benchmark = benchmark_definitions(sagemaker_backend=False)[benchmark_name]
+    benchmark = benchmark_definitions(sagemaker_backend=True)[benchmark_name]
     # These parameters overwrite those given at construction
     plot_params = PlotParameters(
         metric=benchmark.metric,
@@ -60,5 +83,5 @@ if __name__ == "__main__":
     results.plot(
         benchmark_name=benchmark_name,
         plot_params=plot_params,
-        file_name=f"./odsc-comparison-local-{benchmark_name}.png",
+        file_name=f"./odsc-comparison-sagemaker-{benchmark_name}.png",
     )
